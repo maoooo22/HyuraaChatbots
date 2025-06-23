8053721ea15ee8ef1e4bf8fa45f7c40786e1b514
maoooo22 <justmarimo@gmail.com>
2025-06-23 23:17:57 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,2 +1,8 @@
-# HyuraaChatbots
-For marimo
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
